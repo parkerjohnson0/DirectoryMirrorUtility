a4069386544f423cfa7541b9511d9fb9384a3052
Parker Johnson <carverj95@gmail.com>
2022-03-22 08:06:45 -0400

    started on mvvm

---
+++ GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DirectoryWatcher;
+using GUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<MirrorListItem> MirrorListItems { get; set; }
+        public MainViewModel()
+        {
+
+        }
+        public MainViewModel(WatcherService service)
+        {
+
+        }
+    }
+}
